b2678d516ee5e2c2b99e4b7d943d6961bda4e184
guilherme-frt <60244965+guilherme-frt@users.noreply.github.com>
2020-02-03 19:28:19 -0300

    configurando o node

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//task default sass
+gulp.task('sass', async function(){
+
+});
+
+//task default gulp
+gulp.task('default', async function(){
+
+});
