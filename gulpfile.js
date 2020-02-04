cf272832d1b88d1c2db61e255af687b58d31de16
guilherme-frt <60244965+guilherme-frt@users.noreply.github.com>
2020-02-03 23:55:13 -0300

    automatizando css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-//task default sass
-gulp.task('sass', async function(){
+sass.compiler = require('node-sass');
 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.sass')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.sass', ['sass']);
+});
+
 
 //task default gulp
 gulp.task('default', async function(){
